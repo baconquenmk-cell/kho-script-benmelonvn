ff626060e88ea8667eb0b15361a3f99adc82192c
baconquenmk-cell <baconquenmk@gmail.com>
2025-10-29 19:44:08 +0700

    Add files via upload

--- Scriptdemo.lua
+++ Scriptdemo.lua
@@ -3,7 +3,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
        local Window = MakeWindow({
          Hub = {
          Title = "BenMelonVN Studio",
-         Animation = "ben dưa hấu vn studio hello"
+         Animation = "Youtube: BenMelonVN"
          },
         Key = {
         KeySystem = false,
@@ -21,7 +21,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   })
 
        MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=125929030036967",
+       Image = "http://www.roblox.com/asset/?id=98002912463714",
        Size = {60, 60},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
@@ -30,8 +30,12 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
       })
       
 ------ Tab
-     local Tab1o = MakeTab({Name = "Script Farm"})
-     local Tab2o = MakeTab({Name = "Script Game Khác"})
+     local Tab1o = MakeTab({Name = "Blox Fruits"})
+     local Tab2o = MakeTab({Name = "Steal a Brainrot"})
+     local Tab3o = MakeTab({Name = "99 Night in The Froest"})
+     local Tab4o = MakeTab({Name = "Plants vs Brainrot"})
+
+
      
 ------- BUTTON
     
@@ -46,7 +50,32 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/r
   end
   })
 
-   AddButton(Tab2o, {
+    AddButton(Tab1o, {
+     Name = "Hoho Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
+  end
+  })
+
+
+    AddButton(Tab1o, {
+     Name = "Banana Hub (get key)",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+ repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Key = "Input Key" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+  end
+  })
+
+   AddButton(Tab3o, {
    Name = "99 Night In The Froest",
   Callback = function()
 	local Settings = {
@@ -57,3 +86,47 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/m
  end
   })
 
+
+    AddButton(Tab2o, {
+   Name = "Kurd Hub",
+  Callback = function()
+	local Settings = {
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninja10908/S4/refs/heads/main/Kurdhub"))()
+ end
+  })
+
+    AddButton(Tab4o, {
+   Name = "Speed Hub X",
+  Callback = function()
+	local Settings = {
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+ end
+  })
+
+  
+    AddButton(Tab4o, {
+   Name = "Xenith Hub",
+  Callback = function()
+	local Settings = {
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d7be76c234d46ce6770101fded39760c.lua"))()
+ end
+  })
+
+    AddButton(Tab4o, {
+   Name = "UB Hub",
+  Callback = function()
+	local Settings = {
+}
+loadstring(game:HttpGet("https://gitlab.com/r_soft/main/-/raw/main/LoadUB.lua"))()
+  })
+
+    AddButton(Tab4o, {
+   Name = "LDS Hub",
+  Callback = function()
+	local Settings = {
+}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SenhorLDS/ProjectLDSHUB/refs/heads/main/Loader%20LDS%20HUB'))()
+  })
